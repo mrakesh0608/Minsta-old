c6ca0c09903db2253262df4ae68f214eeb9196f7
mrakesh0608 <mrakesh0608@gmail.com>
2022-06-11 08:32:01 +0530

    project init

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-const menu = document.getElementById('menu');
-menu.addEventListener('click',()=>{
-    console.log(menu.id);
-    if(menu.childNodes[0].src === '/icons/menu-on.png'){
-        menu.childNodes[0].src = '/icons/menu.png';
-    }
-    else {
-        console.log(menu.childNodes[0].src);
-        menu.childNodes[0].src = '/icons/menu-on.png';
-    }
-})
-
 const bottom_btn = document.getElementsByClassName('bottom-btn');
 
+Array.from(bottom_btn).forEach(btn =>{
+    btn.addEventListener('click',(e)=>{
+        btn_id = e.target.closest('div').id;
+        showeffect(btn_id);
+    })
+});
+
+var currentTab = 'home';
+const path_i = (document.getElementById(currentTab).childNodes[0].src).indexOf('icons/');
+const path = (document.getElementById(currentTab).childNodes[0].src).substring(0,path_i+6)
+console.log(path);
+
+function showeffect(id){
+    console.log(id);
+    document.getElementById(currentTab).childNodes[0].src =  path + currentTab + '.png';
+    document.getElementById(id).childNodes[0].src =  path + id + '-on.png';
+    currentTab = id;
+}
+
+/*
 Array.from(bottom_btn).forEach(btn =>{
     btn.addEventListener('click',(e)=>{
         btn_id = e.target.closest('div').id;
@@ -23,8 +31,9 @@ function showeffect(list,id){
     console.log(id);
     for(i=0; i<list.length; i++){
         if( list[i].closest('div').id === id){
-            list[i].childNodes[0].src = 'Minsta'+'/icons/'+ id + '-on.png';
+            list[i].childNodes[0].src = '/icons/'+ id + '-on.png';
         }
-        else list[i].childNodes[0].src = 'Minsta'+'/icons/'+ list[i].closest('div').id + '.png';
+        else list[i].childNodes[0].src = '/icons/'+ list[i].closest('div').id + '.png';
     }
 }
+*/
